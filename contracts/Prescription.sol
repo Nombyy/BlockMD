a08a08ae8c03c89f3933533ddb2f50ba70b8b767
faizan <faizan.chikhlikar@gmail.com>
2019-06-01 11:37:01 -0400

    basic barebones

---
+++ contracts/Prescription.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.5.8;
+
+contract Prescription {
+    // Store candidate
+    // Read candidate
+    string public patient;
+
+    // Constructor
+    constructor () public {
+        patient = "Candidate 1";
+    }
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Election = artifacts.require("./Election.sol");
+var Prescription = artifacts.require("./Prescription.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Election);
+  deployer.deploy(Prescription);
 };
