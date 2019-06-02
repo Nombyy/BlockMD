124e7dd21db304227c7ee8449183d36e61b96cfc
faizan <faizan.chikhlikar@gmail.com>
2019-06-01 21:07:38 -0400

    integrated app.js, index.html (please dont modify) and contracts

--- contracts/Doctor.sol
+++ contracts/Doctor.sol
@@ -2,57 +2,72 @@ pragma solidity ^0.5.8;
 
 contract Doctor {
     // Store patient accounts
-    mapping(uint => Patient) public patients;
+    mapping(string => Patient) public patients;
     uint public patientCount;
 
     mapping(uint => Prescription) public prescriptions;
     uint public prescriptionCount;
+    uint public prescriptionID;
+    string public healthCard;
 
     struct Patient {
         uint id;
         string firstName;
         string lastName;
-        uint healthCardNumber;
+        string healthCardNumber;
         uint prescId;
     }
 
     struct Prescription {
-        uint     id;
-        uint   patientHealthCardNumber;
-        string   drugName;
-        string   frequency;
-        string   dosage;
-        bool     patientReceived;
+        uint id;
+        string patientHealthCardNumber;
+        string drugName;
+        string frequency;
+        string dosage;
+        bool patientReceived;
     }
 
     // Constructor
     constructor () public {
         // read from json file
         // create patients
-        addPatient("pavani","Machha",123456789);
-        addPrescription(123456789, "Advil", "Daily", "10 ml");
+        addPatient("Pavani", "Machha", "123456789");
+        addPrescription("123456789", "Advil", "Daily", "10 ml");
+        addPatient("Nhi", "Vuong", "1234-567-890");
+        addPrescription("1234-567-890", "Caffeine", "Every 2 hours", "1 tablet");
     }
 
-    function addPatient(string memory _fname, string memory _lname, uint _healthCard) public {
+    function addPatient(string memory _fname, string memory _lname, string memory _healthCard) public {
         patientCount++;
         patients[_healthCard] = Patient(patientCount, _fname, _lname, _healthCard, 0);
     }
 
-    function addPrescription(uint _healthCard, string memory _drugName,string memory _frequency,string memory _dosage)public
+    function addPrescription(string memory _healthCard, string memory _drugName,string memory _frequency,string memory _dosage)public
     {
         prescriptionCount ++;
         prescriptions[prescriptionCount] = Prescription(prescriptionCount,_healthCard,_drugName,_frequency,_dosage,false);
         patients[_healthCard].prescId = prescriptionCount;
     }
 
-    function fulfillPrescription(uint _healthCard) public {
-        uint prescID = patients[_healthCard].prescId;
-        uint healthCard = prescriptions[prescID].patientHealthCardNumber;
-        //require(!)
-        require(_healthCard != healthCard, "Invalid Patient ID");
+     /* function fulfillPrescription(string memory _healthCard) public {
+        //string memory prescID = patients[_healthCard].prescId;
+        //string memory healthCard = prescriptions[prescID].patientHealthCardNumber;
+
+        prescriptionID = patients[_healthCard].prescId;
+        healthCard = prescriptions[prescriptionID].patientHealthCardNumber;
+
+        require(keccak256(abi.encodePacked(healthCard))!=keccak256(abi.encodePacked(_healthCard)), "Invalid Patient ID");
         // Fulfill
-        prescriptions[_healthCard].patientReceived = true;
+        prescriptions[prescriptionID].patientReceived = true;
+    }  */
+
+    function fulfill(uint presid) private
+    {
+        prescriptions[presid].patientReceived = true;
     }
-
-
+    function fulfillPres() public
+    {
+        //Note:you need to validate the patient ID
+        fulfill(1);
+    }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,92 @@
 App = {
   web3Provider: null,
   contracts: {},
+  account: '0x0',
+  isReceived: false,
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
-
+    // TODO: refactor conditional
+    if (typeof web3 !== 'undefined') {
+      // If a web3 instance is already provided by Meta Mask.
+      App.web3Provider = web3.currentProvider;
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Specify default instance if no web3 instance provided
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+      web3 = new Web3(App.web3Provider);
+    }
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
-
-    return App.bindEvents();
+    $.getJSON("Doctor.json", function(doctor) {
+      // Instantiate a new truffle contract from the artifact
+      App.contracts.Doctor = TruffleContract(doctor);
+      // Connect provider to interact with contract
+      App.contracts.Doctor.setProvider(App.web3Provider);
+      return App.render();
+    });
   },
 
+  render: function() {
+    var instance;
+    var loader = $("#loader");
+    var content = $("#content");
+
+    loader.show();
+    //content.hide();
+
+    // Load account data from Ganache
+    /* web3.eth.getCoinbase(function(err, account) {
+      if (err === null) {
+        App.account = account;
+        $("#accountAddress").html("Pharmacist Account: " + account);
+      }
+    }); */
+    ethereum.enable();
+    App.account = web3.eth.accounts[0];
+    // Talk to Metamask from browser
+    $("#accountAddress").html("Pharmacist Account: " + App.account);
+
+    // Load contract data
+  App.contracts.Doctor.deployed().then(function(i) {
+  instance = i;
+  // $("#healthCard").html("123456789");
+  var healthC = $("#healthCard").val();
+  
+  instance.patients(healthC).then(function(patient){ 
+    var presID = patient[4];
+    console.log(presID + " is the curr prescription id");
+    
+    return presID;    
+  }).then(function(presID) {
+      instance.prescriptions(presID).then(function(prescription){
+        var id = prescription[0];
+        var healthCardNumber = prescription[1];
+        var drugName = prescription[2];
+        var frequency = prescription[3];
+        var dosage = prescription[4]; 
+        var patientReceived = prescription[5];
+
+        // Render prescription Result
+        var prescriptionTemplate = "<tr><th>" + drugName + "</th><td>" + frequency + "</td><td>" + dosage + "</td></tr>"
+        //prescriptionResult.append(prescriptionTemplate);
+        var drugN = $("#drugName").html(drugName);
+
+      })
+  })
+}).catch(function(err) {
+  console.error(err);
+});
+
+  },
+    
   bindEvents: function() {
-    $(document).on('click', '.btn-adopt', App.handleAdopt);
+    $(document).on('click', '#btn-received', App.handleAdopt);
   },
 
   markAdopted: function(adopters, account) {
@@ -60,6 +106,7 @@ App = {
   }
 
 };
+
 
 $(function() {
   $(window).load(function() {
